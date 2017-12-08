01cb3e8de7eab8fb16dea1e8d407d9bd43f3c0f7
Rafael Cardoso <rcardoso@tecnologiageo.com.br>
2017-12-08 17:41:40 -0300

    Adicionado layerlist

--- js/app.js
+++ js/app.js
@@ -2,17 +2,32 @@ require([
     "esri/Map",
     "esri/views/SceneView",
     "esri/WebMap",
+    "esri/widgets/LayerList",
+
     "dojo/domReady!"
-], function (Map, SceneView, WebMap) {
+], function (Map,
+    SceneView,
+    WebMap,
+    LayerList
+) {
 
-    var webmap = new WebMap({
-        portalItem: {
-          id: "93ecf4c2d8c34766b94d5a5becbf0cff" // This was created in the "Style a web map" and "Configure pop-ups" design labs
-        }
-      });
+        var webmap = new WebMap({
+            portalItem: {
+                id: "93ecf4c2d8c34766b94d5a5becbf0cff" // This was created in the "Style a web map" and "Configure pop-ups" design labs
+            }
+        });
 
-    var view = new SceneView({
-        container: "viewDiv",
-        map: webmap
+        var view = new SceneView({
+            container: "viewDiv",
+            map: webmap
+        });
+
+
+        var layerList = new LayerList({
+            view: view
+        });
+
+        // Add widget to the top right corner of the view
+        view.ui.add(layerList, "top-right");
+
     });
-});
