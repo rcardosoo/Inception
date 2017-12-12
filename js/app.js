854ca70c563b6c7198300fef50848739f5310653
Rafael Cardoso <rcardoso@tecnologiageo.com.br>
2017-12-12 10:45:41 -0300

    Adiciondo widget para legendas

--- js/app.js
+++ js/app.js
@@ -6,19 +6,21 @@ require([
     "esri/widgets/Search",
     "esri/widgets/BasemapGallery",
     "esri/widgets/Expand",
+    "esri/widgets/Legend",
 
     "dojo/domReady!"
 ], function (Map,
     SceneView,
     WebMap,
-    LayerList,  
+    LayerList,
     Search,
     BasemapGallery,
-    Expand
+    Expand,
+    Legend
 ) {
         var webmap = new WebMap({
             portalItem: {
-                id: "93ecf4c2d8c34766b94d5a5becbf0cff" 
+                id: "93ecf4c2d8c34766b94d5a5becbf0cff"
             }
         });
 
@@ -26,7 +28,7 @@ require([
             container: "viewDiv",
             map: webmap
         });
-        
+
         var layerList = new LayerList({
             view: view
         });
@@ -45,7 +47,27 @@ require([
             content: basemapGallery.container,
             expandIconClass: "esri-icon-basemap"
         });
+
         
+        view.then(function() {
+            var i=0;
+            var layers = Array();
+            while (typeof webmap.layers.getItemAt(i) != 'undefined') {
+                layers[i] = {
+                    layer: webmap.layers.getItemAt(i),
+                    title: "Legenda"
+                };
+                i++;
+            }
+            console.log(webmap.layers);
+            var legend = new Legend({
+                view: view, layerInfos: layers
+            });
+
+            legend.startup();
+            view.ui.add(legend, "bottom-left");
+        });
+
         view.ui.add(searchWidget, {
             position: "top-right",
             index: 0
