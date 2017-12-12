9195685fc7a0d24d1b486ed04ca03b3cbca24eb1
Rafael Cardoso <rcardoso@tecnologiageo.com.br>
2017-12-12 11:11:43 -0300

    Organizando criação de objetos no código

--- js/app.js
+++ js/app.js
@@ -18,65 +18,77 @@ require([
     Expand,
     Legend
 ) {
-        var webmap = new WebMap({
-            portalItem: {
-                id: "93ecf4c2d8c34766b94d5a5becbf0cff"
-            }
-        });
-
-        var view = new SceneView({
-            container: "viewDiv",
-            map: webmap
-        });
-
-        var layerList = new LayerList({
-            view: view
-        });
-
-        var searchWidget = new Search({
-            view: view
-        });
-
-        var basemapGallery = new BasemapGallery({
-            view: view,
-            container: document.createElement("div")
-        });
-
-        var bgExpand = new Expand({
-            view: view,
-            content: basemapGallery.container,
-            expandIconClass: "esri-icon-basemap"
-        });
-
+        var webmap = createWidget("webMap");
+        var view = createWidget("sceneView");
+        var layerList = createWidget("layerList");
+        var searchWidget = createWidget("search");
+        var basemapGallery = createWidget("basemapGallery");
+        var bgExpand = createWidget("expand");
         
         view.then(function() {
-            var i=0;
-            var layers = Array();
-            while (typeof webmap.layers.getItemAt(i) != 'undefined') {
-                layers[i] = {
-                    layer: webmap.layers.getItemAt(i),
-                    title: webmap.layers.title
-                };
-                i++;
-            }
-            console.log(webmap.layers);
-            var legend = new Legend({
-                view: view, layerInfos: layers
-            });
-
+            var legend = createWidget("legend");
             legend.startup();
             view.ui.add(legend, "bottom-left");
         });
-
         view.ui.add(searchWidget, {
             position: "top-right",
             index: 0
         });
-
-
-
         view.ui.add(bgExpand, "top-left");
         view.ui.add(layerList, "top-right");
 
+
+        function createWidget(name) {
+            if (name == 'webMap') {
+                return new WebMap({
+                    portalItem: {
+                        id: "93ecf4c2d8c34766b94d5a5becbf0cff"
+                    }
+                });
+
+            } else if (name == 'sceneView') {
+                return new SceneView ({
+                    container: "viewDiv",
+                    map: webmap
+                });
+
+            } else if (name == 'layerList') {
+                return new LayerList({
+                    view: view
+                });
+
+            } else if (name == 'search') {
+                return new Search({
+                    view: view
+                });
+
+            } else if (name == 'basemapGallery') {
+                return new BasemapGallery({
+                    view: view,
+                    container: document.createElement("div")
+                });
+
+            } else if (name == 'expand') {
+                return new Expand({
+                    view: view,
+                    content: basemapGallery.container,
+                    expandIconClass: "esri-icon-basemap"
+                });
+
+            } else if (name == 'legend') {
+                var i=0;
+                var layers = Array();
+                while (typeof webmap.layers.getItemAt(i) != 'undefined') {
+                    layers[i] = {
+                        layer: webmap.layers.getItemAt(i),
+                        title: webmap.layers.title
+                    };
+                    i++;
+                }
+                return new Legend({
+                    view: view, layerInfos: layers
+                });
+            }
+        }
 
     });
